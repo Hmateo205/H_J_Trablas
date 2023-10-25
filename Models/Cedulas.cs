6c1046e971926a4a7f5053e51dcb89203de79f51
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2023-10-25 08:45:54 -0500

    Correcciones de ejecucion

--- Models/Cedulas.cs
+++ Models/Cedulas.cs
@@ -3,19 +3,19 @@
 namespace H_J_Trablas.Models
 {
     public class Cedulas
-    {public int Id { get; set; }
+    {
+        public int Id { get; set; }
 
         [Required]
         public string? Nombre { get; set; }
-        
+
         [Required]
         public string? Cedula { get; set; }
 
         public bool Soltero { get; set; }
 
-        public bool Casado {  get; set; }
+        public bool Casado { get; set; }
 
-        [Required]
         public string? Hijos { get; set; }
     }
 }
